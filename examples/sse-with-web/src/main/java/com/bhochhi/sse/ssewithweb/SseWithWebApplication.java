7546e69fe450f85798e84a27b2f94d61e560beca
Rupesh Bhochhibhoya <bhochhi@gmail.com>
2019-02-23 10:52:26 -0600

    all possible way to SSE in web mvc

--- examples/sse-with-web/src/main/java/com/bhochhi/sse/ssewithweb/SseWithWebApplication.java
+++ examples/sse-with-web/src/main/java/com/bhochhi/sse/ssewithweb/SseWithWebApplication.java
@@ -2,12 +2,18 @@ package com.bhochhi.sse.ssewithweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 import java.time.LocalTime;
+import java.util.Date;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -25,12 +31,15 @@ public class SseWithWebApplication {
 @RequestMapping("/sse")
 class SSEController {
 
+	private ExecutorService nonBlockingService = Executors.newCachedThreadPool();
+
+
 	@GetMapping
 	public String getHelloWorld(){
 		return "hello world";
 	}
 
-	@GetMapping("/stream-sse-mvc")
+	@GetMapping("/emitter")
 	public SseEmitter streamSseMvc() {
 		SseEmitter emitter = new SseEmitter();
 		ExecutorService sseMvcExecutor = Executors.newSingleThreadExecutor();
@@ -50,5 +59,48 @@ class SSEController {
 		});
 		return emitter;
 	}
+	@GetMapping("/emmiter-non-blocking")
+	public SseEmitter handleSse() {
+		SseEmitter emitter = new SseEmitter();
+
+		nonBlockingService.execute(() -> {
+			try {
+				emitter.send( "From a Response Body Emitter! @ " + new Date());
+				emitter.complete();
+			} catch (Exception ex) {
+				ex.printStackTrace();
+				emitter.completeWithError(ex);
+			}
+		});
+
+		return emitter;
+	}
+
+
+	@GetMapping("/response-body-emitter")
+	public ResponseEntity<ResponseBodyEmitter> handleRbe() {
+		ResponseBodyEmitter emitter = new ResponseBodyEmitter();
+		nonBlockingService.execute(() -> {
+			try {
+				emitter.send(
+						"/response-body-emitter" + " @ " + new Date(), MediaType.TEXT_PLAIN);
+				emitter.complete();
+			} catch (Exception ex) {
+				emitter.completeWithError(ex);
+			}
+		});
+		return new ResponseEntity(emitter, HttpStatus.OK);
+	}
+
+	@GetMapping("/stream-responsebody")
+	public ResponseEntity<StreamingResponseBody> responsebodyStream() {
+		StreamingResponseBody stream = out -> {
+			String msg = "From StreamingResponseBody @ " + new Date();
+			out.write(msg.getBytes());
+		};
+		return new ResponseEntity(stream, HttpStatus.OK);
+	}
+
+
 
 }
