ce199c3ea00e58bce778a6faafef7555d8e8511c
Rupesh Bhochhibhoya <bhochhi@gmail.com>
2019-02-23 00:08:06 -0600

    init

---
+++ examples/sse-with-web/src/main/java/com/bhochhi/sse/ssewithweb/SseWithWebApplication.java
@@ -0,0 +1,13 @@
+package com.bhochhi.sse.ssewithweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SseWithWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SseWithWebApplication.class, args);
+	}
+
+}
---
+++ examples/sse-with-web/src/test/java/com/bhochhi/sse/ssewithweb/SseWithWebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bhochhi.sse.ssewithweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SseWithWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
